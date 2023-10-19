fb5b4af02d2198e4ad2f28ad70b975be16f95252
JudithA487 <143568356+JudithA487@users.noreply.github.com>
2023-10-18 19:46:44 -0600

    Add files via upload

---
+++ EVA2_10_CALIFAS_EUA/src/eva2_10_califas_eua/EVA2_10_CALIFAS_EUA.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_10_califas_eua;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_10_CALIFAS_EUA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int califa;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Introduce calificación númerica");
+        califa = input.nextInt();
+        
+        switch(califa){
+            case 100:
+                System.out.println("A");
+                break;
+            case 90:
+                System.out.println("A");
+                break;
+            case 89:
+                System.out.println("B");
+                break;
+            case 80:
+                System.out.println("B");
+                break;
+            case 79:
+                System.out.println("C");
+                break;
+            case 70:
+                System.out.println("C");
+                break;
+            case 69:
+                System.out.println("D");
+                break;
+                
+            case 60:
+                System.out.println("D");
+                break;
+            case 59:
+                System.out.println("F");
+                break;
+                
+               
+                                
+
+                
+                    } 
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
