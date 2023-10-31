09e6df8c2bdfdc27b8f1a50170957bb10a78dbca
JudithA487 <143568356+JudithA487@users.noreply.github.com>
2023-10-30 19:48:56 -0600

    Add files via upload

---
+++ EVA2_13_CICLO_FOR/src/eva2_13_ciclo_for/EVA2_13_CICLO_FOR.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_13_ciclo_for;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_13_CICLO_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //for ---> desde
+        //punto de inicio,
+    }
+    
+}
